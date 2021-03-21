3a611d460cf380fbe78d0527f076b1555d8de76f
李光耀 <583143445@qq.com>
2021-03-21 23:55:15 +0800

    高级控件初始化

---
+++ app/src/main/java/com/example/datastorage/contentProvider/ContentResolverActivity.java
@@ -0,0 +1,16 @@
+package com.example.datastorage.contentProvider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.datastorage.R;
+
+public class ContentResolverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_resolver);
+    }
+}
